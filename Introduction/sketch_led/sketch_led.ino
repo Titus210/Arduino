a62c3d11085d5a4a3dcadbd54933c6eb056bb214
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-03-23 04:35:35 +0300

    Sktech

--- Introduction/sketch_led/sketch_led.ino
+++ Introduction/sketch_led/sketch_led.ino
@@ -6,18 +6,48 @@
 
 // Pin 13 gas led light connected to most arduino boards
 // initializing
+
 int led_13 = 13;
+int led_12 = 12;
+int led_11 = 11;
+int change = 0;
 
 
 //  setup routine runs once when press reset
 void setup() {
-  pinMode(led_light, Output);
+  pinMode(led_13, OUTPUT);
+  pinMode(led_11, OUTPUT);
+  pinMode(led_12, OUTPUT);
 }
 
 //
 void loop() {
-  digitalWrite(led_13, HIGH);  // turns the LED on (HIGH is voltage level)
-  delay(1000);                 // waits for a second
-  digitalWrite(led, LOW) l     //turn the LED off by making the voltage LOW
-  delay(1000);
+
+
+  for(change = 0; change <= 10;change++){
+      digitalWrite(led_13, HIGH);  // turns the LED on (HIGH is voltage level)
+    delay(200);                 // waits for a second
+  digitalWrite(led_13, LOW);   //turn the LED off by making the voltage LOW
+  delay(90);
+
+  // switch the lights alternatively 
+
+
+  // if green is high switch orange and red twice on alternatively
+
+    if(change = 30){
+    digitalWrite(led_12, HIGH);
+    delay(500);
+    digitalWrite(led_12, LOW);
+    delay(500);
+    digitalWrite(led_11, HIGH);
+    delay(500);
+     digitalWrite(led_11,LOW);
+     delay(1000);
+    }
+   change = 0;
+
 }
+
+
+  }
