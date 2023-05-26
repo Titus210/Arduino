ffd6cda47012b780647f6c62666897b4b491a623
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-26 10:20:08 +0300

    feat: folder structure and file initialization

---
+++ Smart Home Automation/smartHome.ino
@@ -0,0 +1,13 @@
+/**
+ * @file smartHome.ino
+ * @author 
+ * @brief Creating smart home using nodeMCu esp8266
+ * @version 0.1
+ * @date 2023-05-26
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+/* Create a setup menu to control lights dth-100 and fan using google assistance*/
+/* declare input and output fields*/
